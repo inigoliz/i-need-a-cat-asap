7c3b6b870dc50f2bcbf16bbd05a00bdd0878c2d7
inigoliz <inilariz@gmail.com>
2022-02-20 20:33:00 +0100

    refactored

---
+++ find_my_cat.py
@@ -0,0 +1,127 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+import smtplib
+from email.message import EmailMessage
+import ssl
+import yaml
+
+import os
+import sys
+import re
+
+
+def send_email(new_cats):
+
+    with open('config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    msg = EmailMessage()
+    msg['From'] = config['sender_email']
+    msg['To'] = config['receiver_mail']
+    msg['Subject'] = 'New cats!'
+    msg.set_content("""\
+    New kities ({}) are waiting!
+    Check out the website here:
+    {}.""".format(new_cats, url))
+
+    password = config['password']
+
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(config['sender_email'], password)
+        server.send_message(msg)
+
+
+def save(names, memory_file):
+    with open(memory_file, 'w') as f:
+        for name in names:
+            f.write("%s\n" % name)
+
+
+def initiate_chrome():
+    is_RPi = False
+
+    if "pi" in file_dir:
+        is_RPi = True
+    else:
+        is_RPi = False
+
+    print(f'Executing on RapsberryPi = {is_RPi}')
+
+    if is_RPi:
+        service = Service('/usr/lib/chromium-browser/chromedriver')
+    else:
+        service = Service(file_dir + '/chromedriver')
+
+    options = Options()
+    options.headless = True
+    return webdriver.Chrome(options=options, service=service)
+
+
+def read(memory_file):
+    if not os.path.isfile(memory_file):
+        open(memory_file, 'w')
+    with open(memory_file, 'r') as f:
+        list_contents = f.read().splitlines()
+    return list_contents
+
+
+def get_cats_on_website():
+
+    driver = initiate_chrome()
+    driver.get(url)
+    cats = driver.find_elements(By.XPATH, '//div[@class="view-content-info"]')
+
+    cat_names = []
+    for cat in cats:
+        cat_info = cat.get_attribute('innerHTML')
+        # gets the content inside quotes
+        cat_names.append(re.findall(r'"\/(.*?[^\\])"', cat_info)[0])
+
+    return cat_names
+
+
+def new_cats_in_shelter(memory_file):
+    now = set(get_cats_on_website())
+    before = set(read(memory_file))
+    save(now, memory_file)
+
+    new_cats = now - before
+    new_cats = [cat.capitalize() for cat in new_cats]
+    new_cats = ', '.join(new_cats)
+
+    return new_cats
+
+
+def main():
+    url_flat = 'https://kattens-vaern.dk/adoption?field_internat_tid=All&field_race_tid=All&field_environment_tid=16&field_gender_value=All&field_cat_age_name_tid=All'
+    url_all = 'https://kattens-vaern.dk/adoption?field_internat_tid=All&field_race_tid=All&field_environment_tid=17&field_gender_value=All&field_cat_age_name_tid=All'
+
+    global file_dir
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(file_dir)
+
+    global url
+    url = url_all
+    new_cats = new_cats_in_shelter('cats_available_all.txt')
+
+    if len(new_cats):
+        send_email(new_cats)
+        print(new_cats)
+
+    url = url_flat
+    new_cats = new_cats_in_shelter('cats_available_flat.txt')
+
+    if len(new_cats):
+        send_email(new_cats)
+        print(new_cats)
+
+
+if __name__ == "__main__":
+    main()
