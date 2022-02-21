adb75c2466c63d296737da32b4289aff092911e1
inigoliz <inilariz@gmail.com>
2022-02-21 01:32:44 +0100

    add shell file for crontab

---
+++ find_cat.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script to initiate the virtual environment and run the code
+
+source ~/Documents/Venvironments/venv_INeedACat/bin/activate
+
+python3 ~/Documents/Proyects/INeedACat/find_my_cat.py
